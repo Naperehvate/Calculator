f0e9ad6f7a9c7020dd5925a34ac49abd252794b6
Naperehvate <tropikano647@mail.ru>
2024-07-29 14:22:35 +0500

    Add btn

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultField;
+    TextView operationField;
+    EditText numberField;
+    Double operand = null;
+    String lastOperation = "=";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        resultField = findViewById(R.id.resultField);
+        operationField = findViewById(R.id.operationField);
+        numberField = findViewById(R.id.numberField);
+        findViewById(R.id.btnadd).setOnClickListener(view -> onOperationClick("+"));
+        findViewById(R.id.btnsub).setOnClickListener(view -> onOperationClick("-"));
+        findViewById(R.id.btnmult).setOnClickListener(view -> onOperationClick("*"));
+        findViewById(R.id.btndiv).setOnClickListener(view -> onOperationClick("/"));
+        findViewById(R.id.btnres).setOnClickListener(view -> onOperationClick("="));
+        findViewById(R.id.btn0).setOnClickListener(view -> onNumberClick("0"));
+        findViewById(R.id.btn1).setOnClickListener(view -> onNumberClick("1"));
+        findViewById(R.id.btn2).setOnClickListener(view -> onNumberClick("2"));
+        findViewById(R.id.btn3).setOnClickListener(view -> onNumberClick("3"));
+        findViewById(R.id.btn4).setOnClickListener(view -> onNumberClick("4"));
+        findViewById(R.id.btn5).setOnClickListener(view -> onNumberClick("5"));
+        findViewById(R.id.btn6).setOnClickListener(view -> onNumberClick("6"));
+        findViewById(R.id.btn7).setOnClickListener(view -> onNumberClick("7"));
+        findViewById(R.id.btn8).setOnClickListener(view -> onNumberClick("8"));
+        findViewById(R.id.btn9).setOnClickListener(view -> onNumberClick("9"));
+        findViewById(R.id.btndot).setOnClickListener(view -> onNumberClick("."));
     }
 }
