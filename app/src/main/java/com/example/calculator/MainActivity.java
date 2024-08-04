b64c9ec16322d90176b3e85b6b9328b515eae4dc
Naperehvate <tropikano647@mail.ru>
2024-08-04 14:39:22 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.jetbrains.annotations.NotNull;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,4 +44,81 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn9).setOnClickListener(view -> onNumberClick("9"));
         findViewById(R.id.btndot).setOnClickListener(view -> onNumberClick("."));
     }
+
+    public void onNumberClick(String number) {
+        numberField.append(number);
+        if(lastOperation.equals("=") && operand != null) {
+            operand = null;
+        }
+    }
+    public void onOperationClick(String operation) {
+        String number = numberField.getText().toString();
+        if(number.length() > 0) {
+            number = number.replace(",", ".");
+            try {
+                performOperation(Double.valueOf(number), operation);
+            } catch (Exception e) {
+                numberField.setText("");
+            }
+        }
+        lastOperation = operation;
+        operationField.setText(lastOperation);
+    }
+    private void performOperation(Double number, String operation) {
+        if(operand == null) {
+            operand = number;
+        }
+        else {
+            if(lastOperation.equals("=")) {
+                lastOperation = operation;
+            }
+            switch(lastOperation) {
+                case "=":
+                    operand = number;
+                    break;
+                case "/":
+                    if(number == 0) {
+                        operand = 0.0;
+                    }
+                    else {
+                        operand /= number;
+                    }
+                    break;
+                case "*":
+                    operand *= number;
+                    break;
+                case "+":
+                    operand += number;
+                    break;
+                case "-":
+                    operand -= number;
+                    break;
+                default:
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace(",", "."));
+        numberField.setText("");
+    }
+
+    //сохранение состояния приложения при сворачивании, изменении ориентации, блокировке и тд.
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if(operand != null) {
+            outState.putDouble("OPERAND", operand);
+        }
+        super.onSaveInstanceState(outState);
+    }
+    //восстановление состояние после продолжения работы приложения/смены ориентации
+    @Override
+    protected void onRestoreInstanceState(@NotNull Bundle savedState) {
+        super.onRestoreInstanceState(savedState);
+        lastOperation = savedState.getString("OPERATION");
+        operand = savedState.getDouble("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
+
+
 }
